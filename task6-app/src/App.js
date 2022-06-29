421ecd4afc172acc704f97ba33c614191d5989f6
akashsoni0786 <akashsoni@cedcoss.com>
2022-06-29 16:33:56 +0530

    Make the following design as shown.

---
+++ task6-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Compo1,Compo2,Compo3 } from './Compo';
+function App() {
+  return (
+    <div className="App"> 
+       <Compo1 />
+       <Compo2 />
+       <Compo3 />
+    </div>
+  );
+}
+
+export default App;
