1b80467b46aabf385085533aeaf029af6643ec0d
akashsoni0786 <akashsoni@cedcoss.com>
2022-06-29 16:32:47 +0530

    Make the following component structure using class components.

---
+++ task4-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Compo1,Compo2,Compo4} from './ClassCompo';
+import './App.css';
+
+function App() {
+  return (
+       <div className='container'>
+        <h2>React</h2>
+        <Compo1 />
+        <Compo2 />
+        <Compo4 />
+        </div>
+  );
+}
+
+export default App;
